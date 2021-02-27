c2caee40c9af9d5d9c07f61a5129e05b07c90ff2
Bill Barekas <bb@wappier.com>
2021-02-27 15:50:54 +0200

    Add support for different formats.

--- src/chapter4.rs
+++ src/chapter4.rs
@@ -10,14 +10,18 @@ extern crate serde_json;
 extern crate base64;
 #[macro_use]
 extern crate base64_serde;
+extern crate queryst;
+extern crate serde_cbor;
 
 mod color;
 
 use std::ops::Range;
 use std::cmp::{max, min};
+use failure::Error;
 use futures::{future, Future, Stream};
-use hyper::{Body, Error, Method, Request, Response, Server, StatusCode};
+use hyper::{Body, Method, Request, Response, Server, StatusCode};
 use hyper::service::service_fn;
+use serde_json::Value;
 use rand::Rng;
 use rand::distributions::{Bernoulli, Normal, Uniform};
 use base64::STANDARD;
@@ -60,6 +64,13 @@ enum RngRequest {
     },
 }
 
+#[derive(Deserialize)]
+#[serde(rename_all = "lowercase")]
+enum Format {
+    Json,
+    Cbor,
+}
+
 fn color_range(from: u8, to: u8) -> Uniform<u8> {
     let (from, to) = (min(from, to), max(from, to));
     Uniform::new_inclusive(from, to)
@@ -93,27 +104,44 @@ fn handle_request(request: RngRequest) -> RngResponse {
     }
 }
 
+fn serialize(format: &str, resp: &RngResponse) -> Result<Vec<u8>, Error> {
+    match format {
+        "json" => {
+            Ok(serde_json::to_vec(resp)?)
+        },
+        "cbor" => {
+            Ok(serde_cbor::to_vec(resp)?)
+        },
+        _ => {
+            Err(format_err!("unsupported format {}", format))
+        },
+    }
+}
+
 fn microservice_handler(req: Request<Body>)
-                        -> Box<Future<Item=Response<Body>, Error=Error> + Send>
+                        -> Box<Future<Item=Response<Body>, Error=hyper::Error> + Send>
 {
     match (req.method(), req.uri().path()) {
         (&Method::GET, "/") | (&Method::GET, "/random") => {
             Box::new(future::ok(Response::new(INDEX.into())))
         },
         (&Method::POST, "/random") => {
-
+            let format = {
+                let uri = req.uri().query().unwrap_or("");
+                let query = queryst::parse(uri).unwrap_or(Value::Null);
+                query["format"].as_str().unwrap_or("json").to_string()
+            };
             let body = req.into_body().concat2()
-                .map(|chunks| {
-                    println!("Chunk: {:?}", chunks);
+                .map(move |chunks| {
                     let res = serde_json::from_slice::<RngRequest>(chunks.as_ref())
                         .map(handle_request)
-                        .and_then(|resp| serde_json::to_string(&resp));
+                        .map_err(Error::from)
+                        .and_then(move |resp| serialize(&format, &resp));
                     match res {
                         Ok(body) => {
                             Response::new(body.into())
                         },
                         Err(err) => {
-                            println!("Error: {}", err.to_string());
                             Response::builder()
                                 .status(StatusCode::UNPROCESSABLE_ENTITY)
                                 .body(err.to_string().into())
